a7b76888984e8e1e358ddf1154e1b6cbd357824b
pc20211081 <115874378+pc20211081@users.noreply.github.com>
2022-10-15 18:52:02 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    if (state == "0") {
+        state = 1
+    } else {
+        state = 0
+    }
+})
+let state = 0
+let second = 5
+state = 0
+loops.everyInterval(1000, function () {
+	
+})
+basic.forever(function () {
+	
+})
