6b851fff91d0649895674c1ed44f9c8720111126
pc20211081 <115874378+pc20211081@users.noreply.github.com>
2022-10-17 23:30:03 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,84 @@
 input.onButtonPressed(Button.A, function () {
-    if (state == "0") {
+    if (state == 0) {
         state = 1
     } else {
         state = 0
     }
 })
-let state = 0
-let second = 5
-state = 0
-loops.everyInterval(1000, function () {
-	
+input.onButtonPressed(Button.B, function () {
+    state = 0
+    second = 10
 })
+let state = 0
+let second = 0
+second = 10
+state = 0
 basic.forever(function () {
-	
+    if (state == 1) {
+        if (second > 0) {
+            second += -1
+            basic.showNumber(second)
+            basic.pause(1000)
+            if (second == 0) {
+                state = 0
+                basic.pause(2000)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . . # . .
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . . # . .
+                    . . # . .
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+                    `)
+                basic.showLeds(`
+                    . . . . .
+                    . . # . .
+                    . # # # .
+                    . . # . .
+                    . . . . .
+                    `)
+                basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # # . # #
+                    . # # # .
+                    . . # . .
+                    `)
+                basic.showLeds(`
+                    . # # # .
+                    # # . # #
+                    # . . . #
+                    # # . # #
+                    . # # # .
+                    `)
+                basic.showLeds(`
+                    # # . # #
+                    # . . . #
+                    . . . . .
+                    # . . . #
+                    # # . # #
+                    `)
+            }
+        }
+    }
 })
